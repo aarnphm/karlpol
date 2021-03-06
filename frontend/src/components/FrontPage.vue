c18f707c27ce3ec803f43aa1688090fddc6f44f2
marlon4dashen <alienlql@hotmail.com>
2021-03-06 09:28:24 -0500

    frontpage
    updates package.json

--- frontend/src/components/FrontPage.vue
+++ frontend/src/components/FrontPage.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="FrontPage">
-    <h1> FrontPage </h1>
+    
+    <div class="card">
+
+        <h1> Welcome to Karlpol</h1>
+
+        <h2> Please login: </h2>
+        <form @submit.prevent="login">
+            <label for="Username"> Username:  </label>
+            <input type="text" name="username" v-model="username" placeholder="Enter here">
+            <label for="Username"> Password:  </label>
+            <input type="text" name="password" v-model="password" placeholder="Enter here">
+            <p v-if="feedback" class="fb">{{ feedback }}</p>
+            <button class="button">Start Your Carpool Experience</button>
+        </form>
+
+        <a href="/#/signup">Don't have an account?</a>
+    </div>
+    
+
   </div>
 </template>
 
@@ -8,6 +26,21 @@
 
 export default {
   name: 'FrontPage',
+
+  data(){
+    return {
+      username: null,
+      password: null,
+      feedback: null,
+    }
+  },
+
+  methods: {
+    login(){
+      
+    }
+
+  }
 
 }
 </script>
